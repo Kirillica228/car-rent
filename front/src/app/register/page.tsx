6ea059ccdd308cda31f6fb2b6123219d6b5749a2
Kirillica228 <kirillicar8@gmail.com>
2025-09-10 00:03:59 +0400

    Write register

--- front/src/app/register/page.tsx
+++ front/src/app/register/page.tsx
@@ -1,5 +1,88 @@
-export default function Rgister() {
-    return (
-    <div>Тут типо регистриаця</div>
-    );
+"use client";
+import { useState } from "react";
+
+export default function Register() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirm, setConfirm] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  async function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    setError("");
+    setLoading(true);
+
+    if (password !== confirm) {
+      setError("Пароли не совпадают");
+      setLoading(false);
+      return;
+    }
+
+    try {
+      const res = await fetch("http://localhost:8080/api/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (!res.ok) {
+        const errData = await res.json();
+        throw new Error(errData.error || "Ошибка регистрации");
+      }
+
+      alert("Регистрация успешна! Теперь войдите.");
+    } catch (err: any) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return (
+    <div className="flex flex-col items-center ">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-[16px]">
+          <label htmlFor="email">Эл. почта</label>
+          <input
+            type="email"
+            name="email"
+            className="border p-1 rounded-md"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <label htmlFor="password">Пароль</label>
+          <input
+            type="password"
+            name="password"
+            className="border p-1 rounded-md"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <label htmlFor="confirm">Подтвердите пароль</label>
+          <input
+            type="password"
+            name="confirm"
+            className="border p-1 rounded-md"
+            value={confirm}
+            onChange={(e) => setConfirm(e.target.value)}
+          />
+
+          {error && <p className="text-red-500">{error}</p>}
+
+          <button
+            type="submit"
+            className="bg-green-500 text-white p-2 rounded-md mt-2"
+            disabled={loading}
+          >
+            {loading ? "Регистрируем..." : "Register"}
+          </button>
+        </form>
+      </main>
+    </div>
+  );
 }
